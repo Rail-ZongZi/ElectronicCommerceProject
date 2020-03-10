e102b80cc836767e2064cfd98fb5915e0711326d
粽子 <rail_0039@163.com>
2020-03-11 00:12:21 +0800

    项目目录搭建

---
+++ src/components/NavFooter/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav_footer">底部</div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+  name: 'NavFooter',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang='scss' rel='stylesheet/scss'>
+</style>
